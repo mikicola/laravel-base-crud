69296151e9622263fbb8518c1defb4356e01ae32
mikicola <michela.colangelo@gmail.com>
2022-05-09 17:56:05 +0200

    aggiungo dati

--- database/migrations/2022_05_09_133753_create_comics_table.php
+++ database/migrations/2022_05_09_133753_create_comics_table.php
@@ -15,7 +15,14 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 250)->nullable();
+            $table->integer('price');
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 50);
         });
     }
 
--- resources/views/guest/comicsPage.blade.php
+++ resources/views/guest/comicsPage.blade.php
@@ -1,1 +1,10 @@
-comicsPage
+@extends('guest.templates.base')
+{{--
+1   title = cosa sotituisco
+2   laravel model-controller = cosa inserisco --}}
+@section('title', 'Comics bellissimi')
+
+@section('content')
+<main>
+    <h1>Comics</h1>
+</main>
